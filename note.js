4fcae9b843598ae2023d4aeb8595b036cfaed158
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2022-09-16 22:35:37 +0545

    note app

--- note.js
+++ note.js
@@ -6,6 +6,8 @@ let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
 
     let addTxt = document.getElementById('addTxt');
+    let titleTxt = document.getElementById('titleTxt');
+
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         hello = [];
@@ -13,12 +15,16 @@ addBtn.addEventListener("click", function (e) {
     else {
         hello = JSON.parse(notes);
     }
-    hello.push(addTxt.value);
+    hello.push([addTxt.value,titleTxt.value]);
     localStorage.setItem("notes", JSON.stringify(hello));
     addTxt.value = "";
+    titleTxt.value = "";
+
     console.log(hello);
     showNotes();
 });
+
+
 
 
 function showNotes() {
@@ -30,11 +36,11 @@ function showNotes() {
         hello = JSON.parse(notes);
     }
     let html = "";
-    hello.forEach(function (element, index) {
+    hello.forEach(function (notetitle,element, index) {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">note${index + 1}</h5>
+            <h5 class="card-title">${notetitle}</h5>
             <p class="card-text">${element}</p>
             <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">delete</button>
         </div>
@@ -68,18 +74,24 @@ function deleteNote(index) {
 }
 
 let search = document.getElementById('searchTxt');
-search.addEventListener('input', function () {
+search.addEventListener("input", function () {
 
     let inputVal = search.value.toLowerCase();
     // console.log("input is fire", inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if (cardTxt.includes(inputVal)) {
-            element.style.dispaly = "block";
-        }
-        else {
-            element.style.dispaly = "none";
+        // if (cardTxt.includes(inputVal)) {
+        //     element.style.dispaly = "block";
+        // }
+        // else {
+        //     element.style.dispaly = "none";
+        // }
+        // console.log(cardTxt);
+        if(cardTxt.includes(inputVal)){
+            element.style.display="block";
+        }else{
+            element.style.display="none";
         }
     })
 })
